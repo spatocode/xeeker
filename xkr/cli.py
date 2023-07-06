37fa7dde3019e71e4237f053659c91649eb292ac
Ekene Izukanne <ekeneizukanne@gmail.com>
2023-07-06 15:19:19 +0100

    view usage by network interface

--- xkr/cli.py
+++ xkr/cli.py
@@ -2,10 +2,11 @@ import platform
 import sys
 
 import click
+import pandas as pd
 import psutil
 
 from xkr import __version__
-from xkr.main import system_data_usage
+from xkr.main import system_data_usage, interface_data_usage, process_data_usage
 
 def version_msg():
     os = platform.system().lower()
@@ -15,19 +16,31 @@ def version_msg():
 @click.command(context_settings=dict(help_option_names=['-h', '--help']))
 @click.version_option(__version__, '-V', '--version', message=version_msg())
 @click.argument('command')
-@click.option('-pid', '--process', required=False, help='The process id')
-@click.option('-i', '--interface', required=False, help='The network interface')
+@click.option('-pid', '--process', is_flag=True, required=False, help='The process id')
+@click.option('-i', '--interface', is_flag=True, required=False, help='The network interface')
 @click.option('-p', '--period', default='daily', help='The data usage period of time')
 def main(command, process, interface, period):
     if not command or command.lower() != "usage":
         click.echo(click.get_current_context().get_help())
         sys.exit(0)
 
-    if process and (not process.isnumeric() or not psutil.pid_exists(int(process))):
-        click.echo(f"Process ID '{process}' does not exist in current running processes")
-        sys.exit(0)
+    if process:
+        if not process.isnumeric() or not psutil.pid_exists(int(process)):
+            click.echo(f"Process ID '{process}' does not exist in current running processes")
+            sys.exit(0)
+        click.echo(process_data_usage(period))
+        return
 
-    click.echo(system_data_usage(period))
+    if interface:
+        data = interface_data_usage(period, interface)
+        df = pd.DataFrame(data)
+        click.echo(df.to_string())
+        return
+
+    total, download, upload = system_data_usage(period)
+    click.echo(f"Total: {total}          "
+            f"Download: {download}          "
+            f"Upload: {upload}")
 
 
 if __name__ == "__main__":
--- xkr/main.py
+++ xkr/main.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import psutil
 
 def _parse_byte(bytes):
@@ -7,9 +6,25 @@ def _parse_byte(bytes):
             return f"{bytes:.2f}{unit}B"
         bytes /= 1024
 
+def process_data_usage(period):
+    pass
+
 def system_data_usage(period):
-    nic = psutil.net_io_counters(pernic=True)
+    nic = psutil.net_io_counters()
+    return _parse_byte(nic.bytes_recv+nic.bytes_sent), _parse_byte(nic.bytes_recv), _parse_byte(nic.bytes_sent)
+
+def interface_data_usage(period, interface):
     data = []
+    nic = psutil.net_io_counters(pernic=True)
+    if nic.get(interface):
+        data.append({
+            "Interface": interface,
+            "Total": _parse_byte(nic[interface].bytes_recv + nic[interface].bytes_sent),
+            "Download": _parse_byte(nic[interface].bytes_recv),
+            "Upload": _parse_byte(nic[interface].bytes_sent)
+        })
+        return data
+
     for iface, iface_io in nic.items():
         data.append({
             "Interface": iface,
@@ -17,5 +32,4 @@ def system_data_usage(period):
             "Download": _parse_byte(nic[iface].bytes_recv),
             "Upload": _parse_byte(nic[iface].bytes_sent)
         })
-        df = pd.DataFrame(data)
-    return df.to_string()
+    return data
